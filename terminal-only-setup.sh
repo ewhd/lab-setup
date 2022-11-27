1e40adf8a34a543167bf439e1b0f36f29db5ffd6
ewhd <ewhd22@gmail.com>
2022-11-26 23:29:29 -0800

    split initial-setup.sh into two, one for gui packages and one for terminal only packages

---
+++ terminal-only-setup.sh
@@ -0,0 +1,28 @@
+
+# Install the locate command
+apt install plocate -y
+
+# Install xdg-utils
+apt install xdg-utils -y
+
+
+# Install vim-gtk (which has things like the +clipboard feature, which just vim doesn't for some reason)
+apt install vim-gtk -y
+
+# Install ranger terminal file manager
+apt install ranger -y
+
+# Install Tmux
+apt install tmux -y
+
+# Install Zsh
+apt install zsh -y
+
+# Install stow
+apt install stow -y
+
+# Install htop
+apt install htop -y
+
+# Install xsel
+apt install xsel -y
